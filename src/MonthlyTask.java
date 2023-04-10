4659906b34461e99b6587f8421a407c0e78ef554
Никита <Ployka322@yandex.ru>
2023-04-10 12:20:32 +0700

    course232

---
+++ src/MonthlyTask.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+
+public class MonthlyTask extends Task{
+    public MonthlyTask(String title, String description, TaskType taskType, LocalDateTime date)throws WrongInputException{
+        super(title,description,taskType,date);
+    }
+
+    @Override
+    public boolean checkOccurrence(LocalDateTime requestedDate) {
+        return getFirstDate().getDayOfMonth()==(requestedDate.getDayOfMonth());
+    }
+}
---
+++ src/TaskNotFoundException.java
@@ -0,0 +1,9 @@
+public class TaskNotFoundException extends Exception{
+    public TaskNotFoundException(){
+    }
+    public TaskNotFoundException(String message){super(message);}
+
+    public boolean getMessange() {
+        return false;
+    }
+}
